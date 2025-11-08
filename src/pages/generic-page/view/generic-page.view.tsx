79e2f6919163db951b84dd5643948057800130c8
GabrielDeFreitas <gabrielfreitas.ilustra@gmail.com>
2025-11-08 17:58:27 -0300

    feat: wrap React.Fragment for consistency across views

--- src/pages/generic-page/view/generic-page.view.tsx
+++ src/pages/generic-page/view/generic-page.view.tsx
@@ -1,6 +1,11 @@
+import React from 'react'
 import { TitleGeneric } from '../../../components'
 import type { GenericPageProps } from '../types'
 
 export function GenericPageView({ title }: GenericPageProps) {
-  return <TitleGeneric title={title} />
+  return (
+    <React.Fragment>
+      <TitleGeneric title={title} />
+    </React.Fragment>
+  )
 }
